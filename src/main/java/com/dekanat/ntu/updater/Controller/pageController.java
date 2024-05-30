1b038240173214d6658c672f0f56661d1171bd8c
DarkPoul <poulpotter1@gmail.com>
2024-05-30 13:45:20 +0300

    update html

--- src/main/java/com/dekanat/ntu/updater/Controller/pageController.java
+++ src/main/java/com/dekanat/ntu/updater/Controller/pageController.java
@@ -1,12 +1,28 @@
 package com.dekanat.ntu.updater.Controller;
 
+import com.dekanat.ntu.updater.Entity.VersionInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class pageController {
     @GetMapping("/upload")
     public String getUploadPage() {
         return "upload-files";
+    }
+
+    @GetMapping("/version-info")
+    public String getVersionInfo(Model model) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("/app/version.json");
+        VersionInfo versionInfo = mapper.readValue(file, VersionInfo.class);
+
+        model.addAttribute("version", versionInfo.getVersion());
+        return "version-info";
     }
 }
