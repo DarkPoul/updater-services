47b75c1dbc59acb8d87da59c70eb10a01cbc8d63
DarkPoul <poulpotter1@gmail.com>
2024-06-01 16:54:38 +0300

    update

--- src/main/java/com/dekanat/ntu/updater/Controller/updaterController.java
+++ src/main/java/com/dekanat/ntu/updater/Controller/updaterController.java
@@ -2,7 +2,7 @@ package com.dekanat.ntu.updater.Controller;
 
 import com.dekanat.ntu.updater.Entity.VersionInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -16,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -33,43 +34,22 @@ public class updaterController {
 
         System.out.println(versionInfo.getVersion());
 
-        return "redirect:/download";
+        return "redirect:/version-info";
 
     }
 
-    @GetMapping("/download-update")
-    public ResponseEntity<Resource> downloadUpdate() throws IOException {
-        Path filePath = Paths.get("/app/dekanat.exe");
+    @GetMapping("/download")
+    public ResponseEntity<Resource> download() throws IOException {
+        Path filePath = Paths.get("/app/update.zip");
         if (Files.exists(filePath)) {
-            byte[] fileContent = Files.readAllBytes(filePath);
-            Resource file = new ByteArrayResource(fileContent);
+            Resource file = new InputStreamResource(Files.newInputStream(filePath));
 
             HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=dekanat.exe");
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=update.zip");
 
             return ResponseEntity.ok()
                     .headers(headers)
-                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                    .body(file);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(null);
-        }
-    }
-
-    @GetMapping("/download-installer")
-    public ResponseEntity<Resource> downloadInstaller() throws IOException {
-        Path filePath = Paths.get("/app/DekanatInstaller.exe");
-        if (Files.exists(filePath)) {
-            byte[] fileContent = Files.readAllBytes(filePath);
-            Resource file = new ByteArrayResource(fileContent);
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=DekanatInstaller.exe");
-
-            return ResponseEntity.ok()
-                    .headers(headers)
-                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .contentType(MediaType.parseMediaType("application/x-zip-compressed"))
                     .body(file);
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
@@ -78,47 +58,33 @@ public class updaterController {
     }
 
     @PostMapping("/upload-update")
-    public ResponseEntity<String> uploadUpdate(
+    public ResponseEntity<Void> uploadUpdate(
             @RequestParam("jsonFile") MultipartFile jsonFile,
-            @RequestParam("installerFile") MultipartFile installerFile,
-            @RequestParam("updateFile") MultipartFile updateFile) {
+            @RequestParam("exeFile") MultipartFile exeFile) {
+
         try {
             // Обробка завантаженого JSON файлу
-            if (jsonFile != null && !jsonFile.isEmpty()) {
-                Path jsonFilePath = Paths.get("/app/" + jsonFile.getOriginalFilename());
-                Files.createDirectories(jsonFilePath.getParent());
-                Files.write(jsonFilePath, jsonFile.getBytes());
-            } else {
-                throw new IllegalArgumentException("JSON файл не був завантажений");
-            }
+            Path jsonFilePath = Paths.get("/app/" + jsonFile.getOriginalFilename());
+            Files.createDirectories(jsonFilePath.getParent());
+            Files.write(jsonFilePath, jsonFile.getBytes());
 
-            // Обробка завантаженого інсталятора
-            if (installerFile != null && !installerFile.isEmpty()) {
-                if (!Objects.equals(installerFile.getContentType(), "application/vnd.microsoft.portable-executable")) {
-                    throw new IllegalArgumentException("Невірний тип файлу для інсталятора");
-                }
-                Path installerFilePath = Paths.get("/app/", installerFile.getOriginalFilename());
-                Files.write(installerFilePath, installerFile.getBytes());
-            } else {
-                throw new IllegalArgumentException("Інсталятор не був завантажений");
+            // Обробка завантаженого .exe файлу
+            if (exeFile != null && !exeFile.isEmpty() &&
+                    (Objects.equals(exeFile.getContentType(), "application/octet-stream") ||
+                            Objects.equals(exeFile.getContentType(), "application/x-msdownload"))) {
+                Path exeFilePath = Paths.get("/app/" + exeFile.getOriginalFilename());
+                Files.createDirectories(exeFilePath.getParent());
+                Files.write(exeFilePath, exeFile.getBytes());
             }
-
-            // Обробка завантаженого оновлення (EXE файл)
-            if (updateFile != null && !updateFile.isEmpty()) {
-                if (!Objects.equals(updateFile.getContentType(), "application/vnd.microsoft.portable-executable")) {
-                    throw new IllegalArgumentException("Невірний тип файлу для оновлення");
-                }
-                Path updateFilePath = Paths.get("/app/", updateFile.getOriginalFilename());
-                Files.write(updateFilePath, updateFile.getBytes());
-            } else {
-                throw new IllegalArgumentException("Оновлення не було завантажено");
-            }
-
-            return ResponseEntity.ok("Файли успішно завантажені");
-        } catch (IOException | IllegalArgumentException ex) {
-            ex.fillInStackTrace(); // Логуємо стек-трейс для отримання інформації про помилку
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Сталася помилка під час завантаження файлів");
+        } catch(IOException ex) {
+            ex.fillInStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/version-info"));
+
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 
 }
